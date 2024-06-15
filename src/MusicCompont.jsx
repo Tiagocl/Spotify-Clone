2cf1f47724ccc205cdd37aad5abe6bbb6654efd9
Tiagocl <up202004374@up.pt>
2024-06-15 20:40:18 +0100

    added cards(bug in the animation)

--- src/MusicCompont.jsx
+++ src/MusicCompont.jsx
@@ -4,6 +4,7 @@ import PlaylistMain from './PlaylistMain';
 import React, { useState } from 'react';
 import PlaylistCard from './PlaylistCard';
 
+
 export default function MusicComponent() {
 
     const [activeButton, setActiveButton] = useState(null);
@@ -72,7 +73,7 @@ export default function MusicComponent() {
 
             </div>
             <div className="card-container">
-            <div className="made-for-you">
+            <div className="card-span">
                 <span>Made For You</span>
             </div>
             <div className="cards">
@@ -80,15 +81,45 @@ export default function MusicComponent() {
                     img="src/assets/img1.png"
                     artists="XXXTENTACION, Juice WRLD, Logic and more"
                 />
-                <PlaylistCard name="Daily Mix 1"
+                <PlaylistCard name="Daily Mix 2"
                     img="src/assets/img1.png"
-                    artists="XXXTENTACION, Juice WRLD, Logic and more"
+                    artists="Nagalli, L7NNON, Pk and more"
                 />
-                <PlaylistCard name="Daily Mix 1"
+                <PlaylistCard name="Daily Mix 3"
                     img="src/assets/img1.png"
-                    artists="XXXTENTACION, Juice WRLD, Logic and more"
+                    artists="Wet Bed Gand, Yuri NR5, LON3r JOHNY and more"
                 />
+                <PlaylistCard name="Daily Mix 4"
+                    img="src/assets/img1.png"
+                    artists="Litrox, Alcool Club, Dillaz and more"
+                />
+
             </div>
+            </div>
+            <div className="card-container">
+                <div className="card-span">
+                    <span>
+                        Recommended Stations
+                    </span>
+                <div className="cards">
+                <PlaylistCard name="Greg Ferreira Radio"
+                    img="src/assets/img1.png"
+                    artists="With Jon Vlogs, Matt Fuze, MISAEL and more"
+                />
+                <PlaylistCard name="Pop Smoke Radio"
+                    img="src/assets/img1.png"
+                    artists="With Lil TJay, Polo G, Roddy Ricch and more"
+                />
+                <PlaylistCard name="Travis Scott Radio"
+                    img="src/assets/img1.png"
+                    artists="With will.i.am, Fergie, Timbaland and more"
+                />
+                <PlaylistCard name="21 Savage Radio"
+                    img="src/assets/img1.png"
+                    artists="With Metro Boomin, Gunna, Future and more"
+                />
+                </div>
+                </div>
             </div>
             </div>
         </div>
--- src/PlaylistCard.jsx
+++ src/PlaylistCard.jsx
@@ -1,20 +1,33 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { useHover } from 'react-use';
 
 export default function PlaylistCard(props) {
-
-    return(
+    const [hoverable, isHovered] = useHover((hovered) => (
         <div className="card">
-            <div className="card-img">
+            <div className={`card-img ${hovered ? 'hovered' : ''}`}
+            style={{ position: hovered ? 'relative' : 'static' }}>
                 <img src={props.img} alt="Playlist image" />
+                <div className={`card-play ${hovered ? 'show' : ''}`}>
+                    <i id="play-icon" className="bi bi-play-fill"></i>
+                </div>
             </div>
-            <div className="card-name">
-                <span>{props.name}</span>
-            </div>
-            <div className="artistas">
-                <span>{props.artists}</span>
+            <div className="text-card">
+                <div className="card-name">
+                    <span>{props.name}</span>
+                </div>
+                <div className="artists">
+                    <span>{props.artists}</span>
+                </div>
             </div>
         </div>
-    );
+    ));
+
+    return hoverable;
 }
 
-PlaylistCard.PropTypes
+// Add PropTypes definition
+PlaylistCard.propTypes = {
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    artists: PropTypes.string.isRequired
+};
